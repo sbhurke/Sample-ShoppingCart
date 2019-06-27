bb566b4928ffc82981b622915de75a0a45113cb9
S Bhurke <sonali_bhurke@yahoo.com>
2019-06-26 18:16:57 -0700

    Updates

---
+++ precache-manifest.a67d463d74daf3418dc0ee427ba7171e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3d49336b6515f2456ed9e816018666",
+    "url": "/Sample-ShoppingCart/index.html"
+  },
+  {
+    "revision": "8cf30a4223fd8cb6a29f",
+    "url": "/Sample-ShoppingCart/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "05d734d720d8fb757f97",
+    "url": "/Sample-ShoppingCart/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8cf30a4223fd8cb6a29f",
+    "url": "/Sample-ShoppingCart/static/js/2.eb088328.chunk.js"
+  },
+  {
+    "revision": "05d734d720d8fb757f97",
+    "url": "/Sample-ShoppingCart/static/js/main.8db48c07.chunk.js"
+  },
+  {
+    "revision": "55b931f92acc5f6c39fc",
+    "url": "/Sample-ShoppingCart/static/js/runtime~main.cf3a83a3.js"
+  }
+]);
